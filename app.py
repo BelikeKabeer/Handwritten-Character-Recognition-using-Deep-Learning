43d9594e9608bdbf843fc0bf4d607378067aad72
Kabeer Munshipalkar <64410823+BelikeKabeer@users.noreply.github.com>
2025-08-16 01:31:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -151,5 +151,5 @@ def upload():
     except Exception as e:
         return jsonify({"success": False, "error": str(e)})
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
